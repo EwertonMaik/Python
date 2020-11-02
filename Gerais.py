50fb695a2debed03cb0b7d1c06d170df94e4c37b
Ewerton Maik <ewertonmsa@hotmail.com>
2020-11-02 15:31:57 -0300

    Update Gerais.py

--- Gerais.py
+++ Gerais.py
@@ -12,7 +12,40 @@ C:\PythonFundamentos>python hello.py
 # É possível executar o script dentro do shell(Interpretador) do Python, iniciando o console do Python
 python
 
+# Pequeno exemplo calculando e escrevendo valores em tela
 >>> 2 + 2
 4
 >>>("Hello World!")
 Hello World!
+
+# Operações Aritiméticas
+2 + 2  # SOMA
+10 - 5 # SUBTRAÇÃO
+20 / 2 # DIVISÃO
+6 ** 2 # POTÊNCIA
+10 % 2 # MÓDULO (RESTO DA DIVISÃO)
+
+# Operações com Função Built-In
+type(10)   #Rertorna o tipo INT
+type(10.0) #Retorna o tipo FLOAT
+a = 'Texto String';
+type(a)    #Retorna o tipo STRING
+
+# Operações com FLOAT
+# Float com Float retorna um Float
+# Float com Int retorna Float
+# Int com Int retorna Int ou Float dependendo da operação de / ou *
+
+# Operação de conversão
+float(7) # Converte para 7.0
+int(5.0) # Converte para 5
+int(5.5) # Converte para 5, pega só a parte inteira do Nº
+
+# Operação coversão para Hexadecimal e Binário
+hex(568) #Hexadecimal
+bin(622) #Binário
+
+# Operações com Funções
+abs(-9) # Retorna valor absoluto
+round(3.14151922, 2) # Retorna o Nº aredondado para 2 casa decimais
+pow(5, 2) # Retorna potência
