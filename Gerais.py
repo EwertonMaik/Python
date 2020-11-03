9e8198c8cb5a9183eb2f9e6efd3cf0bbc5d99cc4
Ewerton Maik <ewertonmsa@hotmail.com>
2020-11-03 16:39:55 -0300

    Update Gerais.py

--- Gerais.py
+++ Gerais.py
@@ -18,6 +18,10 @@ python
 >>>("Hello World!")
 Hello World!
 
+# Váriável Declarada e impressa com função print
+varString = "Pedro"
+print(varString)
+
 # Operações Aritiméticas
 2 + 2  # SOMA
 10 - 5 # SUBTRAÇÃO
@@ -49,3 +53,19 @@ bin(622) #Binário
 abs(-9) # Retorna valor absoluto
 round(3.14151922, 2) # Retorna o Nº aredondado para 2 casa decimais
 pow(5, 2) # Retorna potência
+
+# Declaração Multipla de Variável
+var01, var02, var03 = "Jose", "Carlos", "Silva"
+
+# Cálculo entre variáveis
+largura = 2
+altura = 4
+area = largura * altura
+print(area)
+
+# Concatenação de String
+nome = "Pedro"
+sobrenome = "Paulo"
+nomeCompleto = nome + " " + sobrenome
+print(nomeCompleto)
+nomeCompleto # A variável é impressa simplesmente sem a função print também
