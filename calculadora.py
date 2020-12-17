50a7f9a2d37117d5d884091ab1cfdbf0fe0f7f47
Ewerton Maik <ewertonmsa@hotmail.com>
2020-12-16 21:13:38 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -23,36 +23,36 @@ def sair():
 
 while var01 == 0:
         print("1 - SOMA / 2 - SUBTRAÇÃO / 3 - MULTIPLICAÇÃO / 4 - DIVISÃO / 0 - SAIR CALC.")
-        var02 = int(input("Digite o Nº operação desejada: "))
+        var02 = input("Digite o Nº operação desejada: ")
         
-        var03 = int(input("Digite nº 01 : ") )
-        var04 = int(input("Digite nº 02 : ") )
-        
-        if var02 == 1:
-            #chama função soma
-            result = opSoma(var03, var04)
-            print('Resultado : %r' %(result) )
-            
-        elif var02 == 2:
-            #chama função subtração
-            result = opSubtracao(var03, var04)
-            print('Resultado : %r' %(result) )
-            
-        elif var02 == 3:
-            #chama função multiplicação
-            result = opMultiplicacao(var03, var04)
-            print('Resultado : %r' %(result) )
-            
-        elif var02 == 4:
-            #chama função divisão
-            result = opDivisao(var03, var04)
-            print('Resultado : %r' %(result) )
-        
-        elif var02 == 0:
+        if var02 == "0":
             #Sai o Programa
             sair()
             print("Calculadora Encerrada!")
+            
+        elif var02 == "1" or var02 == "2" or var02 == "3" or var02 == "4":
+            var03 = int(input("Digite nº 01 : ") )
+            var04 = int(input("Digite nº 02 : ") )
         
+            if var02 == "1":
+                #chama função soma
+                result = opSoma(var03, var04)
+                print('Resultado : %r' %(result) )
+
+            elif var02 == "2":
+                #chama função subtração
+                result = opSubtracao(var03, var04)
+                print('Resultado : %r' %(result) )
+
+            elif var02 == "3":
+                #chama função multiplicação
+                result = opMultiplicacao(var03, var04)
+                print('Resultado : %r' %(result) )
+
+            elif var02 == "4":
+                #chama função divisão
+                result = opDivisao(var03, var04)
+                print('Resultado : %r' %(result) )
         else:
-            print("Número Inválido! Digite a opção correta!")
+            print("Por favor, Digite um opção correta! de (0 à 4)")
             
