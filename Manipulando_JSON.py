b5d7c6b8c5a477f3fb2795e254d14864c8c26360
Ewerton Maik <ewertonmsa@hotmail.com>
2020-12-26 17:46:13 -0300

    Update Manipulando_JSON.py

--- Manipulando_JSON.py
+++ Manipulando_JSON.py
@@ -14,3 +14,18 @@ for k,v in dict.itens(): # Função do Dicionário para cada Chave / Valor
 
 # Para manipular arquivos JSON, é utilizado a biblioteca json
 import json
+
+# utilizando uma função JSON para converter um Dicionário em json
+json.dumps(dict)
+
+# Criando um arquivo JSON - Modo w- writer
+with open('arquivos/dados.json','w') as arquivo:
+  arquivo.write(json.dumps(dict) ) # É escrito no arquivo o dodos do dicionário que são convertidos para json com a função dumps
+
+# Abrindo o arquivo json anterior, agora em modo r - read
+with open('arquivos/dados.json','r') as arquivo:
+  texto = arquivo.read() # Variável texto recebe leitura do arquivo json
+  data = json.loads(texto) # Variável data carrega os dados da variável texto em formato jason 
+
+print(data)
+
