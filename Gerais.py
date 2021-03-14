25ea29ad580f3ff4d338a639c9a6180ae6819f82
Ewerton Maik <ewertonmsa@hotmail.com>
2021-03-14 10:12:11 -0300

    Update Gerais.py

--- Gerais.py
+++ Gerais.py
@@ -122,27 +122,4 @@ print("Python" == "R")
 print("Python" == "Python")
 >>> True
 
-# Modulos e Pacotes
-Repositório de Pacotes do Python - Pypi - Python Package Index - htttps://pypi.python.org/pypi
-import pacote.modulo
-import math -- Importanto Biblioteca matemática - todas as funções do pacote
-from mathimport sqrt -- Importando apenas uma função especifica do pacote math
-
-cond list -- Comando cmd para listar todos os pacotes que estão instalados na instalação de sua instância Python
-
--- Comando cmd para para realizar instalação de um pacote Python
-conda install "nome_do_modulo"
-pip install "nome_do_modulo"
-pip install python-pptx -- Pacote que cria e manipular arquivos do Microsoft Power Point a partir do Python.
-keras 2.1.5 - Pacote para trabalhar com Deep Learning For Humans
-
-
-
-
-
-
-
-
-
-
 
