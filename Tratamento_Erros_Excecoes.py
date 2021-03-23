8ddf3f76c4b6c17a03eb8d313e35aff0ac0dbafa
Ewerton Maik <ewertonmsa@hotmail.com>
2021-03-23 20:38:58 -0300

    Create Tratamento_Erros_Excecoes.py

---
+++ Tratamento_Erros_Excecoes.py
@@ -0,0 +1,6 @@
+-- Tratamento de Erros e Exceções
+-- Lista de Biblioteca Exceções Python - https://docs.python.org/3.6/library/exceptions.html
+  
+-- Execução da linha obtem erro devido a falta da 2º ASPAS Simples
+print('Olá)
+-- SyntaxError: EOL while scanning string literal
