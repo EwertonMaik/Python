0b25f13508af7cf75183ba52d1af5ce4c618229c
Ewerton Maik <ewertonmsa@hotmail.com>
2021-05-16 14:38:13 -0300

    Create Criando_Banco_MongoDB.py

--- Python_MongoDB/Criando_Banco_MongoDB.py
+++ Python_MongoDB/Criando_Banco_MongoDB.py
@@ -2,9 +2,10 @@
 
 ## Comando de instalação do pacote para trabalhar e conectar com o MongoDB
 ## Comando deve conter a exclamação, pois faz parte da execução e instalação do SHELL
+## pip é um pacote de instalção de programas do Python
 !pip install -q pymongo
 
-## Realizando a importação do MongoClient
+## Realizando a importação da função MongoClient que conecta ao Banco MongoDB
 from pymongo import MongoClient
 
 ## Criando instância de objeto que conecta ao Banco de Dados MongoDB e passando os parâmetros de host e porta
@@ -14,4 +15,6 @@ conn = MongoClient('localhost', 27017)
 type(conn)
 
 ## OBS um Banco ou uma Collection do MongoDB só é criando efetivamente, quando o primeiro registros é inserido
+## Uma instância permite a criação de diversos Bancos de Dados
 db = conn.cadastradodb
+
