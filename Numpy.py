24caa603992163c2d5b7824744a54b50ece3b9ba
Ewerton Maik <ewertonmsa@hotmail.com>
2021-06-19 15:39:49 -0300

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,10 @@
+## Conhecendo o Pacote Numpy e suas Operações
+
+## Instalando o pacote Numpy
+pip install numpy -U
+
+## Importando pacote Numpy
+import numpy as np
+
+## Consultando versão do pacote
+np.__version__
