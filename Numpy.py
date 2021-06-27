d897d4376e8ca85965b544f0f703e0d1ea48adc4
Ewerton Maik <ewertonmsa@hotmail.com>
2021-06-27 20:35:44 -0300

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -8,3 +8,15 @@ import numpy as np
 
 ## Consultando versão do pacote
 np.__version__
+
+## Função help para listar sintaxe da função
+help(np.array)
+
+## Criando Array a partir de uma lista
+vetor01 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+## Imprimindo o vetor
+print(vetor01)
+
+## Usando função type para lista o tipo do Objeto
+type(vetor01)
