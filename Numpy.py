84de08fb149a6b4b71cfaea02defdf744f2c22f4
Ewerton Maik <ewertonmsa@hotmail.com>
2021-06-27 21:50:55 -0300

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -20,3 +20,21 @@ print(vetor01)
 
 ## Usando função type para lista o tipo do Objeto
 type(vetor01)
+
+## Função que retorna a soma cumulativa dos elementos do vetor
+vetor01.cumsum()
+
+## Criando uma Lista de valores
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+## Imprimindo o tipo do Objeto
+type(lista)
+
+## Imprimindo o valor de uma posição 5 do Vetor
+print(vetor01[5])
+
+## Atribuindo um novo valor a posição 5 do vetor
+vetor01[5] = 100
+
+## Função que retorna quantas dimensões e elementos o vetor possui
+print(vetor01.shape)
