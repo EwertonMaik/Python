851b3afb5fc43eafd6e5868e35b664dd73ed7c42
Ewerton Maik <ewertonmsa@hotmail.com>
2022-01-12 23:09:18 -0300

    Add files via upload

---
+++ Python_PowerBI/Python_PowerBI.py
@@ -0,0 +1,14 @@
+-- Script documentação Python + Power BI para Analise de Dados
+
+Biblioteca Python para Analise de Dados (Py Data Stack)
+
+matplotlib - https://matplotlib.org
+ 
+seaborn - https://seaborn.pydata.org
+
+pandas - pandas.pydata.org - Excel
+
+numpy - numpy.org - Operações matemática de dados
+
+Instalação do Interpretador Python
+anaconda.com (Possui o Interpretador + biblioteca dos pacotes Python)
