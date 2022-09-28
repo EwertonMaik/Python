4d183c19167429d16ed231bacdbf457779d710fe
Ewerton Maik <ewertonmsa@hotmail.com>
2022-09-27 22:07:05 -0300

    Update Projeto_IMDB_01.py

--- Python_Analise_IMDB_Filmes/Projeto_IMDB_01.py
+++ Python_Analise_IMDB_Filmes/Projeto_IMDB_01.py
@@ -178,3 +178,75 @@ generos_percentual = 100 * pd.Series(generos.sum()).sort_values(ascending = Fals
 generos_percentual.head(10)
 
 # Plot
+plt.figure(figsize = (16,8))
+sns.barplot(x = generos_percentual.values, y = generos_percentual.index, orient = "h", palette = "terrain")
+plt.ylabel("Gênero")
+plt.xlabel("\nPercentual de Filmes (%)")
+plt.title("\nNúmero de Títulos Por Gênero\n")
+plt.show()
+
+
+# 3 - Qual a Mediana de Avaliação dos Filmes Por Gênero
+# Mediana de avaliação dos filmes
+
+# Consulta SQL
+consulta3 = '''
+SELECT rating, genres FROM
+ratings JOIN titles ON ratings.title_id = titles.title_id
+WHERE premiered <= 2022 AND type = 'movie'
+'''
+
+# Resultado
+resultado3 = pd.read_sql_query(consulta3, conn)
+
+# Visualizando
+display(resultado3)
+
+# Vamos criar uma função para retornar os genêros
+def retorna_generos(df):
+          df['genres'] = df['genres'].str.lower().values
+          temp = df['genres'].dropna()
+          vetor = CountVectorizer(token_pattern = '(?u)\\b[\\w-]+\\b', analyzer = 'word').fit(temp)
+          generos_unicos = vetor.get_feature_names()
+          generos_unicos = [genre for genre in generos_unicos if len(genre) > 1 ]
+          return generos_unicos
+
+# Aplica a função
+generos_unicos = retorna_generos(resultado)
+
+# Visualiza
+generos_unicos
+
+# Cria lista vazias
+genero_counts = []
+genero_ratings = []
+
+# Loop
+for item in generos_unicos:
+          # Retorna a contagem de filmes por gênero
+          consulta = 'SELECT COUNT(rating) FROM ratings JOIN titles ON ratings.title_id = titles.title_id WHERE genres LIKE ' + '\'\+'%'+item+'%'+'\' AND type=\ 'movie\''
+          resultado = pd.read_sql_query(consulta, conn)
+          genero_counts.append(resultado.values[0][0] )
+          
+          # Retorna a avaliação de filmes por gênero
+          consulta = 'SELECT rating FROM ratings JOIN titles ON ratings.title_id = titles.title_id WHERE genres LIKE ' + '\''+'%'+item+'%'+'\' AND type=\ 'movie\''
+          resultado = pd.read_sql_query(consulta, conn)
+          genero_ratings.append(np.median(resultado['rating']) )
+
+# Prepara o DataFrame Final
+df_genero_ratings = pd.DataFrame()
+df_genero_ratings['genres'] = generos_unicos
+df_genero_ratings['count'] = genero_counts
+df_genero_ratings['rating'] = genero_ratings
+
+# Drop do Índice 18 (news)
+# Não queremos essa informação commo gẽnero
+df_genero_ratings  = df_genero_ratings.drop(index = 18)
+
+# Ordena o resultado
+df_genero_ratings = df_genero_ratings.sort_values(by = 'ratings', ascending = False)
+
+# Plot
+
+# Figura
+plt.figure(figsize = (16,10) )
