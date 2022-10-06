fc1f175482c3cf83cdbb2fd3aa1dc07e9dac571b
Ewerton Maik <ewertonmsa@hotmail.com>
2022-10-06 16:24:22 -0300

    Update Projeto_IMDB_01.py

--- Python_Analise_IMDB_Filmes/Projeto_IMDB_01.py
+++ Python_Analise_IMDB_Filmes/Projeto_IMDB_01.py
@@ -383,3 +383,65 @@ FROM titles
 WHERE type = 'movie' AND Runtime != 'NaN'
 ORDER BY Runtime DESC
 LIMIT 1
+'''
+
+resultado6 = pd.read_sql_query(consulta6, conn)
+resultado6
+
+# 7 - Qual a Relação Entre Duração e Gênero
+# Relação entre duração do filme e gênero
+
+# Consulta SQL
+consulta7 = '''
+SELECT AVG(runtime_minutes) Runtime, genres
+FROM titles
+WHERE type = 'movie' AND runtime_minutes != 'NaN'
+GROUP BY genres
+'''
+
+# Resultado
+resultado7 = pd.read_sql_query(consulta7, conn)
+
+# Retorna gêneros únicos
+generos_unicos = retorna_generos(resultado7)
+
+# Visualiza
+generos_unicos
+
+# Calcula duração por Gênero
+genero_runtime = []
+for item in generos_unicos:
+          consulta = 'SELECT runtime_minutes Runtime FROM titles WHERE genres LIKE ' + '\'' + '%' + item + '%' + '\' AND type=\'movie\' AND Runtime != \'Nan\''
+          resultado = pd.read_sql_query(consulta, conn)
+          genero_runtime.append(np.median(resultado['Runtime']))
+
+# Prepara o dataframe
+df_genero_runtime = pd.DataFrame()
+df_genero_runtime['genre'] = generos_unicos
+df_genero_runtime['runtime'] = genero_runtime
+
+# Remove índice 18 (NEWS)
+df_genero_runtime = df_genero_runtime.drop(index = 18)
+
+# Ordena os Dados
+df_genero_runtime = df_genero_runtime.sort_values(by = 'runtime', ascending = False)
+
+# Plot
+# Tamanho da figura
+plt.figura(figsize = (16,8))
+
+# Barplot
+sns.barplot(y = df_genero_runtime.genre, x = df_genero_runtime.runtime, orient = "h" )
+
+# Loop
+for i in range(len(df_genero_runtime.index) ):
+          plt.text(df_genero_runtime.runtime[df_genero_runtime.index[i] ] ),
+          i + 0.25,
+          round(df_genero_runtime['runtime'] [df_genero_runtime.index[i]], 2 )
+
+plt.ylabel('Gênero')
+plt.xlabel('\Mediana de Tempo de Duração {Minutos} ')
+plt.title('\nRelação Entre Duração e Gênero\n')
+plt.show()
+
+# 8 - Qual o Número de Filmes Produzidos Por País
