5e040446b6d31a3a6ec6b503b6d6736dd5b14f0d
Ewerton Maik <ewertonmsa@hotmail.com>
2023-01-11 22:48:51 -0300

    Create Matplotlib.py

---
+++ Matplotlib.py
@@ -0,0 +1,13 @@
+## Matplotlib
+
+## Matplotlib é a blibioteca Python mais popular para visualização de dados e geração de gráficos.
+## Matplotlib permite que você crie facilmente gráficos, histogramas e outras visualizações profissionais.
+## Quando usado no Jupyter Notebook, Matplotlib tem recursos interativos com zoom e visão panorâmica.
+## Ele possui suporte em todos os sistemas operacionais e também pode exportar gráficos para vetor comum e outros formatos como:
+## pdf, svg, jpg, png, bmp, gif, etc.
+## Para simples plotagem o método pyplot fornece uma interface parecida com o Matlab, especialmente quando combinado com Jupyter Notebook.
+## o usuário tem total controle de estilos de linhas, propriedades de fonte, atributos, etc.. através de uma interface orientada a objetos
+## ou através de um conjunto de funções familiares aos usuários do Matlab.
+## A dica mais importante quando se trata de plotagem é: menos é mais
+## O objetivo final da visualização de dados, é apresentar um resultado, um insight. E você não precisa de um gráfico poluído para isso.
+## Acredite, visualização de dados é quase uma arte!
