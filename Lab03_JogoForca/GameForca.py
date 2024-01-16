af2e81b4c893a5a5ded611be6dee622c374c68c0
Ewerton Maik <ewertonmsa@hotmail.com>
2024-01-15 23:28:13 -0300

    Create GameForca.py

---
+++ Lab03_JogoForca/GameForca.py
@@ -0,0 +1,15 @@
+# Projeto 1 - Desenvolvimento de Game em Linguagem Python - Versão 1
+
+# Import
+import randon
+from os import system, name
+
+# Função para limpar a tela a cada execução
+def limpar_tela():
+  # Windows
+  if name == 'nt':
+    _ = system('cls')
+
+# Mac ou Linux
+  else:
+    _ = system('clear')
