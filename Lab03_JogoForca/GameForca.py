490eaaaf16b3d4ab1dd9196e5c2f2c472c1c1cc9
Ewerton Maik <ewertonmsa@hotmail.com>
2024-01-16 23:24:21 -0300

    Update GameForca.py

--- Lab03_JogoForca/GameForca.py
+++ Lab03_JogoForca/GameForca.py
@@ -27,40 +27,41 @@ def game():
   # Escolha randomicamente uma palavra
   palavra = rondom.choice(palavras)
 
-# List Comprehension
-letras_descobertas = [ '_' for letra in palavra ]
+  # List Comprehension
+  letras_descobertas = [ '_' for letra in palavra ]
+  
+  # Numero de chances
+  chances = 6
+  
+  # Lista para as letras erradas
+  letras_errada = []
 
-# Numero de chances
-chances = 6
+  # Loop enquanto número de chances for maior do que zero
+  while chances > 0:
+      #Print
+      print(" ".join(letras_descobertas) )
+      print("\nChances restantes:", chances)
+      print("Letras erradas:", " "..join(letras_erradas) )
+  
+      # Tentativa
+      tentativa = input("\nDigite uma çetra: ").lower()
+  
+      # Condicional
+      if tentativa in palavra:
+        index = 0;
+      for letra in palavra:
+        if tentativa == letra:
+          letras_descobertas[index] = letra
+          index += 1
+        else:
+          chances -= 1
+          letras_erradas.append(tentativa)
 
-# Lista para as letras erradas
-letras_errada = []
-
-# Loop enquanto número de chances for maior do que zero
-while chances > 0:
-    #Print
-    print(" ".join(letras_descobertas) )
-    print("\nChances restantes:", chances)
-    print("Letras erradas:", " "..join(letras_erradas) )
-
-    # Tentativa
-    tentativa = input("\nDigite uma çetra: ").lower()
-
-    # Condicional
-    if tentativa in palavra:
-      index = 0;
-    for letra in palavra:
-    if tentativa == letra:
-      letras_descobertas[index] = letra
-    index += 1
-else:
-  chances -= 1
-  letras_erradas.append(tentativa)
-
-    # Condicional
-    if "_" not in letras_descobertas:
-      print("\nVocê venceu, a palavra era: ", palavra)
-      break
+      # Condicional
+      if "_" not in letras_descobertas:
+        print("\nVocê venceu, a palavra era: ", palavra)
+        break
+        
 # Condicional
 if "-" in Letras_descobertas:
   print("\nVocê perdeu, a palavra era: palavra")
