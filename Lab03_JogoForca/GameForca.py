0e5657f1b45aa94e279fa63356e10404c0475eb4
Ewerton Maik <ewertonmsa@hotmail.com>
2024-01-16 23:22:10 -0300

    Update GameForca.py

--- Lab03_JogoForca/GameForca.py
+++ Lab03_JogoForca/GameForca.py
@@ -35,3 +35,37 @@ chances = 6
 
 # Lista para as letras erradas
 letras_errada = []
+
+# Loop enquanto número de chances for maior do que zero
+while chances > 0:
+    #Print
+    print(" ".join(letras_descobertas) )
+    print("\nChances restantes:", chances)
+    print("Letras erradas:", " "..join(letras_erradas) )
+
+    # Tentativa
+    tentativa = input("\nDigite uma çetra: ").lower()
+
+    # Condicional
+    if tentativa in palavra:
+      index = 0;
+    for letra in palavra:
+    if tentativa == letra:
+      letras_descobertas[index] = letra
+    index += 1
+else:
+  chances -= 1
+  letras_erradas.append(tentativa)
+
+    # Condicional
+    if "_" not in letras_descobertas:
+      print("\nVocê venceu, a palavra era: ", palavra)
+      break
+# Condicional
+if "-" in Letras_descobertas:
+  print("\nVocê perdeu, a palavra era: palavra")
+
+# Bloco Main
+if __name__ == "__main__":
+  game()
+  print("\nParabés. Você está aprendendo programação em Python com a DSA. :)\n")
