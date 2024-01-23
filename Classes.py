c6247183e3f1b303e5848734a0eeca229c5a951d
Ewerton Maik <ewertonmsa@hotmail.com>
2024-01-22 23:02:50 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -49,3 +49,19 @@ Livro2.imprime("O Poder do Hábito", 77886611)
 ###########################################################################
 
 # Criando a classe cachorro
+class Algoritmo():
+  def __init__(self, tipo_algo):
+    self.tipo = tipo_algo
+    print("Construtor chamado para criar um objeto desta classe.")
+
+# Criando um objeto a partir da classe
+algo1 = Algoritmo(tipo_algo = 'Random Forest')
+
+# Criando um objeto a partir da classe
+algo2 = Algoritmo(tipo_algo = 'Deep Learning')
+
+# Atributo da classe
+algo1.tipo
+
+# Atributo da classe
+algo2.tipo
